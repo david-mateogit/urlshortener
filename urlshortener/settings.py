3c80dcfbb1db004e154d3fa819bbb9f8567194d9
david-mateogit <davidmateo@outlook.com>
2021-02-09 10:46:36 -0400

    add copy share feature

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -77,10 +77,11 @@ WSGI_APPLICATION = 'urlshortener.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    # }
+    # 'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': "database"
+    }
 }
 
 
